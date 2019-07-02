f11b0565a45545f83846df6ac20dde203b3d0880
Saraskov <43336948+Saraskov@users.noreply.github.com>
2019-07-02 14:50:46 +0200

    Alle eksisterende projekter er oprettet

--- stylesheets/typed_animation.js
+++ stylesheets/typed_animation.js
@@ -1,48 +1,48 @@
 // JavaScript Document
 //Script for the Velkommen text at index.html
- var heading = document.querySelector("h1");
-                var data = ["<h1>Hej jeg er Sara</h1>","<h1>Velkommen til mit portfolio</h1>","<p>Multimediestuderende</p>"];
-                var j = 0;
-                var i = 0;
-    
-
-            var k;
-
-            function typing(){
-                if(heading.innerHTML.length === data[j].length){
-                    k = 0;
-                    setTimeout(del, 1200);
-                }
-                if(i<data[j].length){
-                    heading.innerHTML += data[j].charAt(i);
-                    i++;
-                    setTimeout(typing, 50);
-                }
-                else{
-                    j++;
-                    i = 0;
-                    k = 0;
-                    setTimeout(typing, 3000);
-                }
-                if(j === data.length){
-                    j = 0;
-                }
-            }
-                
-            // var check = document.getElementById("check");
-            // var k = 0;
-            function del(){
-                if(heading.innerHTML.length == 0){
-                    clearTimeout(time);
-                }
-                else{
-                heading.innerHTML = heading.innerHTML.slice(0, heading.innerHTML.length - k);
-                k++;
-                var time = setTimeout(del, 100);
-                }
-            }
-
-            typing();
+//// var heading = document.querySelector("h1");
+////                var data = ["<h1>Hej jeg er Sara</h1>","<h1>Velkommen til mit portfolio</h1>","<p>Multimediestuderende</p>"];
+////                var j = 0;
+////                var i = 0;
+////    
+////
+////            var k;
+////
+////            function typing(){
+////                if(heading.innerHTML.length === data[j].length){
+////                    k = 0;
+////                    setTimeout(del, 1200);
+////                }
+////                if(i<data[j].length){
+////                    heading.innerHTML += data[j].charAt(i);
+////                    i++;
+////                    setTimeout(typing, 50);
+////                }
+////                else{
+////                    j++;
+////                    i = 0;
+////                    k = 0;
+////                    setTimeout(typing, 3000);
+////                }
+////                if(j === data.length){
+////                    j = 0;
+////                }
+////            }
+////                
+////            // var check = document.getElementById("check");
+////            // var k = 0;
+////            function del(){
+////                if(heading.innerHTML.length == 0){
+////                    clearTimeout(time);
+////                }
+////                else{
+////                heading.innerHTML = heading.innerHTML.slice(0, heading.innerHTML.length - k);
+////                k++;
+////                var time = setTimeout(del, 100);
+////                }
+////            }
+////
+////            typing();
 
 
 //Javascript for the image gallery navgation
